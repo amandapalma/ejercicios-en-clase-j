aaeb741bc90c8452eff40e80addefb30e930646c
Miguel del Mazo <migueldelmazo@gmail.com>
2020-06-29 13:26:50 +0200

    Add module 2 weekend fun exercise

--- modulo-2-practica-fin-de-semana-01/app/js/main.js
+++ modulo-2-practica-fin-de-semana-01/app/js/main.js
@@ -1,21 +1,43 @@
 'use strict';
 
-//SELECTORS...
+// SELECTORS...
 
-//Number of user attemps
+const btn = document.querySelector('.js-login-button');
+
+// Number of user attemps
 let attemps = 0;
 
-//Object saved in BBDD with user information to compare with my form
+// Object saved in BBDD with user information to compare with my form
+
 const userInfo = {
-	name: 'front23',
-	password: 'bicicleta'
+  name: 'front23',
+  password: 'bicicleta'
 };
 
-//FUNCTIONS
+// FUNCTIONS
 
+function handleLogin() {
+  // leer los datos
+  const name = document.querySelector('.js-user-name').value;
+  const password = document.querySelector('.js-user-password').value;
+  // limpiar el mensaje de error
+  document.querySelector('.js-error-wrapper').classList.add('hidden');
+  // comprobar los datos
+  if (name === userInfo.name && password === userInfo.password) {
+    alert('Estás dentro');
+    // reinicio los intentos
+    attemps = 0;
+  } else {
+    document.querySelector('.js-error-wrapper').classList.remove('hidden');
+    // aumentar los intentos
+    attemps = attemps + 1;
+    if (attemps > 3) {
+      alert('Estás bloqueado');
+    }
+  }
+  console.log(name, password);
+}
 
+// LISTENERS
 
-
-
-
-//LISTENERS
+btn.addEventListener('click', handleLogin);
